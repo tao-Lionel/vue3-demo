b888774d00390cb49480c211a4e4353a64360dac
wangtao <lionel_tao@163.com>
2022-07-26 09:47:44 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <HelloWorld ref="helloWorld" />
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: { HelloWorld },
+  setup() {
+    const helloWorld = ref(null);
+
+    onMounted(() => {
+      console.log(helloWorld.value);
+    });
+
+    return {
+      helloWorld,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <input ref="divRef" v-model="state1.count" type="text" />
+  <button @click="handleClick">{{ demoComputed }}</button>
+
+  <ul>
+    <li v-for="item in list" ref="itemRefs" :key="item">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref, nextTick, shallowReactive, computed, watch, watchEffect, watchPostEffect, onMounted, Ref, toRefs } from "vue";
+
+// export default {
+//   setup() {
+// 深层响应
+const state1 = reactive({
+  count: 0,
+  arr: [1],
+  f: "wang",
+  l: "tao",
+});
+
+const demoComputed = computed(() => {
+  return state1.arr.length > 0 ? "yes" : "no";
+});
+
+onMounted(() => {});
+
+const list = ref([1, 2, 3, 4, 56]);
+const itemRefs = ref([]);
+
+function handleClick() {
+  state1.count++;
+}
+defineExpose({
+  list,
+});
+
+// return {
+//   ...toRefs(state1),
+//   demoComputed,
+//   list,
+//   itemRefs,
+//   handleClick,
+// };
+//   },
+// };
+</script>
+
+<style scoped></style>
+<!-- <template>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "",
+  components: {},
+  async setup() {
+
+    return {
+    }
+
+  }
+})
+</script> -->
